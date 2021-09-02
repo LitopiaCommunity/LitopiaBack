a75b529209843fd43ea17cd198e9866c38b73989
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-09-02 19:07:28 +0200

    Build : Inisialisation du projet nestjs

--- app/RouteMethode.ts
+++
@@ -1,5 +0,0 @@
-interface RouteMethod {
-    path: string,
-    method: (ctx: any) => void,
-    type: "delete"|"get"|"head"|"options"|"patch"|"post"|"put"
-}
--- main.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "https://deno.land/x/oak@v9.0.0/mod.ts";
-import { testController } from "./testController.ts";
-
-const app = new Application();
-
-new testController('/api/v1', app).init();
-
-await app.listen({port:8000})
