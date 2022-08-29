00af18d56531ac44417a8e07cbe7243e8647cd4c
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-08-29 13:03:44 +0200

    Build : add typeORM and all the needed config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(app.get(ConfigService).get('APP_PORT'));
 }
 bootstrap();
